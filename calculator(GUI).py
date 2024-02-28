979e215f2d32fb87f7695708cec5ba74efe08cad
Shahzil-M4IN <154637129+Shahzil-M4IN@users.noreply.github.com>
2024-02-28 16:48:25 +0500

    Add files via upload

---
+++ calculator(GUI).py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def calc(n1_entry_var, n2_entry_var, operator_val, result_label):
+    try:
+        n1_entry_var.set(n1_entry.get())
+        n2_entry_var.set(n2_entry.get())
+        n1 = n1_entry_var.get()  
+        n2 = n2_entry_var.get()  
+        op = operator_val.get()  
+        if op=='+':
+            result=n1+n2
+        elif op=='-':
+            result=n1-n2
+        elif op=='*':
+            result=n1*n2
+        elif op=='/':
+            result=n1/n2
+        elif op=='%':
+            result=n1%n2
+        result_label.config(text=result)
+    except Exception as e:
+        messagebox.showerror('Alert', f'Error: {e}')
+main=tk.Tk()
+main.title('Calculator')
+main.geometry('650x500')
+n1_label=tk.Label(main,text='Number A:',font=('Xenara',15))
+n1_label.grid(row=0,column=0,padx=5)
+n1_entry=tk.Entry(main,font=('Xenara',15))
+n1_entry.grid(row=1,column=0,padx=5)
+n1_entry_var=tk.IntVar(main)
+n2_label=tk.Label(main,text='Number B:',font=('Xenara',15))
+n2_label.grid(row=0,column=2,padx=5)
+n2_entry=tk.Entry(main,font=('Xenara',15))
+n2_entry.grid(row=1,column=2,padx=5)
+n2_entry_var=tk.IntVar(main)
+operators=['+','-','*','/','%']
+operator_val=tk.StringVar(main)
+operator_val.set('Select an Operation')
+operator_drop=tk.OptionMenu(main,operator_val,*operators)
+operator_drop.grid(row=2,column=1,pady=10)
+result_label=tk.Label(main,font=('Xenara',15),fg='red')
+result_label.grid(row=4,column=1,pady=20)
+calculate=tk.Button(main,text='Calculate',font=('Xenara',15),bg='light grey',command=lambda: calc(n1_entry_var,n2_entry_var,operator_val,result_label))
+calculate.grid(row=3,column=1,pady=20)
+main.mainloop()
